279170cdf9f78223376146ccd5ab788b45a6d152
Unknown <paulinapatrowicz@o2.pl>
2019-03-17 01:48:45 +0100

    Lab3 prototype

---
+++ CSharp Lab3/CSharp Lab3/CSharp Lab3/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CSharp_Lab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.changeFolderBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.currentPathLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // changeFolderBtn
+            // 
+            this.changeFolderBtn.Location = new System.Drawing.Point(608, 21);
+            this.changeFolderBtn.Name = "changeFolderBtn";
+            this.changeFolderBtn.Size = new System.Drawing.Size(117, 32);
+            this.changeFolderBtn.TabIndex = 0;
+            this.changeFolderBtn.Text = "Change Folder";
+            this.changeFolderBtn.UseVisualStyleBackColor = true;
+            this.changeFolderBtn.Click += new System.EventHandler(this.changeFolderBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current directory: ";
+            // 
+            // currentPathLabel
+            // 
+            this.currentPathLabel.AutoSize = true;
+            this.currentPathLabel.Location = new System.Drawing.Point(134, 18);
+            this.currentPathLabel.MaximumSize = new System.Drawing.Size(450, 0);
+            this.currentPathLabel.Name = "currentPathLabel";
+            this.currentPathLabel.Size = new System.Drawing.Size(53, 17);
+            this.currentPathLabel.TabIndex = 2;
+            this.currentPathLabel.Text = "Not set";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.changeFolderBtn);
+            this.groupBox1.Controls.Add(this.currentPathLabel);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(765, 68);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(29, 108);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(0, 17);
+            this.infoLabel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button changeFolderBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label currentPathLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label infoLabel;
+    }
+}
+
---
+++ CSharp Lab3/CSharp Lab3/CSharp Lab3/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSharp_Lab3
+{
+    public partial class Form1 : Form
+    {
+        private string _currentPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _currentPath = AppDomain.CurrentDomain.BaseDirectory;
+        }
+
+        private void StartAnalyze()
+        {
+            string result = FindSlnFiles();
+            if (result != "")
+            {
+                string[] path = result.Split(Path.DirectorySeparatorChar);
+                infoLabel.Text += "Znaleziono plik " + path[path.Length - 1] + "\nAnalizuję...\n";
+
+                result = FindProjFiles();
+                if (result != "")
+                {
+                    path = result.Split(Path.DirectorySeparatorChar);
+                    infoLabel.Text += "Znaleziono plik " + path[path.Length - 1] + "\nAnalizuję...\n";
+                }
+                else
+                {
+                    infoLabel.Text += "Nie znaleziono pliku .*proj\n";
+                }
+            }
+            else
+            {
+                infoLabel.Text += "Nie znaleziono pliku .sln\n";
+            }
+
+        }
+
+        private string FindSlnFiles()
+        {
+            string[] files = Directory.GetFiles(_currentPath, "*.sln");
+
+            if(files.Length != 0) return files[0];
+            return "";
+        }
+
+        private string FindProjFiles()
+        {
+            string[] files = Directory.GetFiles(_currentPath, "*.*proj", SearchOption.AllDirectories);
+
+            if (files.Length != 0) return files[0];
+            return "";
+        }
+
+        private void changeFolderBtn_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+
+            _currentPath = folderBrowserDialog1.SelectedPath;
+            currentPathLabel.Text = _currentPath;
+
+            StartAnalyze();
+        }
+
+    }
+}
